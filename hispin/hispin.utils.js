41107c96c67f7858364e9d116310009e78453e36
chameera01 <chameera9019@gmail.com>
2016-03-14 16:10:25 +0530

    isString(), isNumber(), isInt(), isArray(), isBoolean() added...

--- hispin/hispin.utils.js
+++ hispin/hispin.utils.js
@@ -1,3 +1,113 @@
 /**
  * Created by nhancao on 3/11/16.
  */
+
+/**
+ * return true for string values and string objects
+ * @param arg
+ * @returns {boolean}
+ */
+var isString = function (arg) {
+
+    try {
+        return Object.prototype.toString.call(arg) == '[object String]';
+    } catch (e) {
+        return false;
+    }
+};
+
+/**
+ * return true for number values and number objects
+ * @param arg
+ * @returns {boolean}
+ */
+var isNumber = function (arg) {
+    console.log(arg);
+    try {
+        return (!(arg === Infinity) && Object.prototype.toString.call(arg) == '[object Number]' && !isNaN(arg));
+        //return false;
+    } catch (e) {
+        return false;
+    }
+    //return !isNaN(arg);
+};
+
+/**
+ * return true for integer values and integer objects
+ * @param arg
+ * @returns {*}
+ */
+var isInt = function (arg) {
+    try {
+        return isNumber(arg) && Number.isInteger(Number(arg));
+    } catch (e) {
+        return false;
+    }
+};
+
+/**
+ * return true for arrays
+ * @param arg
+ * @returns {boolean}
+ */
+var isArray = function (arg) {
+    try {
+        return Object.prototype.toString.call(arg) === '[object Array]';
+    } catch (e) {
+        return false;
+    }
+};
+
+/**
+ * return true for booleans
+ * @param arg
+ * @returns {boolean}
+ */
+var isBoolean = function (arg) {
+    try {
+        return Object.prototype.toString.call(arg) == '[object Boolean]';
+    } catch (e) {
+        return false;
+    }
+};
+
+/**
+ *
+ * @param x
+ * @param y
+ * @returns {boolean}
+ */
+Object.equals = function (x, y) {
+    if (x === y) return true;
+// if both x and y are null or undefined and exactly the same
+
+    if (!( x instanceof Object ) || !( y instanceof Object )) return false;
+// if they are not strictly equal, they both need to be Objects
+
+    if (x.constructor !== y.constructor) return false;
+// they must have the exact same prototype chain, the closest we can do is
+// test there constructor.
+
+    for (var p in x) {
+        if (!x.hasOwnProperty(p)) continue;
+        // other properties were tested using x.constructor === y.constructor
+
+        if (!y.hasOwnProperty(p)) return false;
+// allows to compare x[ p ] and y[ p ] when set to undefined
+
+        if (x[p] === y[p]) continue;
+// if they have the same strict value or identity then they are equal
+
+        if (typeof( x[p] ) !== "object") return false;
+// Numbers, Strings, Functions, Booleans must be strictly equal
+
+        if (!Object.equals(x[p], y[p])) return false;
+// Objects and Arrays must be tested recursively
+    }
+
+    for (p in y) {
+        if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) return false;
+        // allows x[ p ] to be set to undefined
+    }
+    return true;
+};
